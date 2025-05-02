7c9fa67f75b0054b846927ceccd7fabeb3497da2
Paul-Raimiel-Gonda <157487413+Paul-Raimiel-Gonda@users.noreply.github.com>
2025-05-03 00:42:29 +0800

    Fixed visibility of possible paths

--- PROJECT ASTARION/ASTARION/ASTARION/obj/Debug/net5.0/ASTARION.AssemblyInfo.cs
+++ PROJECT ASTARION/ASTARION/ASTARION/obj/Debug/net5.0/ASTARION.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASTARION")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+492a0b7d3116673b3b27e49c30e06788d19e56b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b79036fa9210092e89cf4d15cdc88a1e32dda93")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASTARION")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASTARION")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
